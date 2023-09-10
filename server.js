9d347993c7be8e5f95a82404741a649d610917f0
akifbayram <56325239+akifbayram@users.noreply.github.com>
2023-09-10 17:36:32 -0400

    Health check server

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.use(express.static('../IP_fe'));
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/../IP_fe/index.html');
+});
+
+app.get('/health-check', (req, res) => {
+  res.send('Hello World');
+  console.log(`Health check endpoint was hit`);
+});
+
+app.listen(PORT, () => {
+  console.log('Server is running on port ' + PORT);
+});
+
+// https://expressjs.com/en/starter/hello-world.html
